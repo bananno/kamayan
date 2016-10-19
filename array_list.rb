f4bc7b7808363e17a097bd0cffde6aea15ab3376
Anna Peterson <peterson.anna.c@gmail.com>
2016-10-19 13:36:42 -0700

    Week 2

--- array_list.rb
+++ array_list.rb
@@ -2,7 +2,8 @@ class ArrayList
   attr_reader :size
 
   def initialize
-    @array = FixedArray.new(10)
+    @max_size = 10
+    @array = FixedArray.new(@max_size)
     @size = 0
   end
 
@@ -15,14 +16,54 @@ class ArrayList
   # append the argument to the end of this ArrayList and increase the size by
   # 1. The return value must be self.
 
+  def <<(new_item)
+    upsize
+
+    @array[@size] = new_item
+
+    @size += 1
+  end
+
   # Define a method ">>" which takes a single argument. This method should
   # prepend the argument to the beginning of this ArrayList and increase the
   # size by 1. The return value must be self.
+
+  def >>(new_item)
+    upsize
+
+    @size.times do |i|
+      @array[@size] = @array[@size - 1]
+    end
+
+    @array[0] = new_item
+
+    @size += 1
+  end
 
   # Define a "delete" method which takes a single index argument. This method
   # should delete the value at the provided index and return it. The size should
   # be 1 less than it was before this method was called. The index must be
   # within the bounds of the ArrayList, or an IndexError should be raised.
+
+  def delete(index)
+    temp = 0
+
+    @size -= 1
+
+    @size.times do |i|
+
+      if temp == index
+        temp += 1
+      end
+
+      @array[i] = @array[temp]
+
+      temp += 1
+
+    end
+
+    @array[@size] = nil
+  end
 
   # Define a method "[]=" which takes 2 arguments. This method should set the
   # value at the index defined in the first argument such that array_list[index]
@@ -40,7 +81,28 @@ class ArrayList
   # existing index would not affect the size, but an index greater than the last
   # index will add the difference to the size.
 
+  def []=(index, new_value)
+  end
+
+
+
+  # PRIVATE METHODS
+
   private
+
+  def upsize
+    if @size == @max_size
+      temp_array = @array
+
+      @array = FixedArray.new(@max_size + 10)
+
+      @max_size.times do |i|
+        @array[i] = temp_array[i]
+      end
+
+      @max_size += 10
+    end
+  end
 
   def check_bounds(index)
     check_lower_bound(index)
